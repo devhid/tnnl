919701f7a786644d5821874f726b23d2edf90e1b
spiderpig86 <slim679975@gmail.com>
2019-03-27 15:28:21 -0400

    feat(server): Started working on server

---
+++ server/server-spec.md
@@ -0,0 +1,3 @@
+# Tnnl Design Spec: Server
+
+The server is designed to be the **source** for **commanding and controlling** infected clients over a botnet for exfiltrating data and delivering payloads.
---
+++ server/server.py
@@ -0,0 +1,14 @@
+import argparse
+import os
+
+from scapy.all import *
+from sys import argv, exit
+
+class TnnlServer():
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
