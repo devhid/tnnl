d87c03744bf274451825238aafcbb334be8df28a
devhid <devhid@users.noreply.github.com>
2019-03-27 23:23:14 -0400

    Fixed more typos

--- client/command.py
+++ client/command.py
@@ -2,12 +2,12 @@ class Command:
     """ A class for handling the command sent by the C&C server. """
 
     def __init__(self, encoded_command):
-        self.cmd = encrypted_command
+        self.cmd = encoded_command
     
     def decode(self):
-        """ Decodes the encoded command into a bash command. """
+        """ Decodes the encoded command into a shell command. """
         pass
     
     def execute(self):
-        """ Executes the command in a bash environment . """
+        """ Executes the command in a shell environment . """
         pass
