70ab828a3eb5fb925fe9a7c36f80057e23145bc5
devhid <devhid@users.noreply.github.com>
2019-05-04 00:18:42 -0400

    added more debug output

--- client/command/command.py
+++ client/command/command.py
@@ -13,6 +13,7 @@ class Command:
     def __init__(self, encrypted_command):
         self.cmd = encrypted_command
         print("cmd: " + self.cmd)
+        print('type: ' + type(self.cmd))
     
     def execute(self):
         """ Executes the command based on the type of command. """
